d0ca1a97d54f9760c22d1d7aa85e97814ac768af
Shubhankar Deskpande <shubhankardeshpande.14@gmail.com>
2023-11-14 23:50:09 -0500

    Time: 25 ms (21.38%), Space: 13.3 MB (30.69%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -1,9 +1,14 @@
-class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        total_candies = max(candies)
-        bool_list=[]
-        for i in candies:
-            if (i+extraCandies) >= total_candies:
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        candies_ = [i+extraCandies for i in candies]
+        bool_list = []
+        for i in candies_:
+            if  i >= max(candies):
                 bool_list.append(True)
             else:
                 bool_list.append(False)
