4af3b2765eadc40b15da35c7841b5a4ce650e12c
Shubhankar Deskpande <shubhankardeshpande.14@gmail.com>
2024-04-16 00:34:48 -0400

    Time: 48 ms (39.18%), Space: 11.8 MB (41.61%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,28 +1,35 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        head = l3 = ListNode(0)
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
         carry = 0
-        while l2 or l1 or carry:
+        head = None
+        temp = None
+        while l1 or l2:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            value = val1 + val2 + carry
+            carry = value // 10
+            value = value % 10
+            new_node = ListNode(value, None)
+            if not head:
+                head = new_node
+            else:
+                temp.next = new_node
+            temp = new_node
             if l1:
-                carry += l1.val
                 l1 = l1.next
             if l2:
-                carry += l2.val
                 l2 = l2.next
-            l3.val = carry%10
-            carry = carry // 10
-            if l1 or l2 or carry:
-                l3.next = ListNode(0)
-                l3 = l3.next
+        if carry:
+            temp.next = ListNode(carry, None)
         return head
             
-                
-                
-            
-        
-        
